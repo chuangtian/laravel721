f1fcaf19e8aa039ac7c464452ce2e1414e9fee3e
田闯 <1509760688@qq.com>
2021-03-08 15:32:15 +0800

    no message

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -153,15 +153,15 @@ class TestController extends Controller
         $timeout = 60;
         $web3 = new Web3(new HttpProvider(new HttpRequestManager(config('app.eth'), $timeout)));
         $contract = new Contract($web3->provider, $abi);
-        $contractAddress='0x49cbbff45201f887579d0ed5a24917a0a8371d27';
-        $fromAccount = '0xaba10bfe13e4e3bac8babacd4a2a082152fe1313';
-        $password = '!@superpassword';
+        $contractAddress='0xf64c95194c4d6e7b9a5eb6e3e46a8c4f5bfb66b6';
+        $fromAccount = '0xb24bae98610c454e4e2e3e8711d22af3a3155db0';
+        $password = 'vd!LiedNJ9DkGRpA';
         $a = $this->unlockAccount($fromAccount, $password);
         if (!$a) {
             return '解锁失败';
         }
         //调用send('方法','参数','回调')锻造一个币
-        $contract->at($contractAddress)->send('mint', '0xaba10bfe13e4e3bac8babacd4a2a082152fe1313',2,0,'',[
+        $contract->at($contractAddress)->send('mint', '0x3138E4972c9ACd112E5514286B76A58442981f0B',1,'1111111',[
             'from' => $fromAccount,
         ], function ($err, $result){
             dd($err,$result);
